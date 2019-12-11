48d307dbb178ebde23917ee7f8b0ff09d334da7e
Rose <50706764+Edestiny7@users.noreply.github.com>
2019-12-11 18:06:37 -0500

    Changes made so far - answers not working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,32 +1,86 @@
-$(function() {
+$(function () {
+
+    const wins = 0;
+    const losses = 0;
+    const skipped = 0;
+
 
     //  URL needed to query the database
-    var queryURL = "https://opentdb.com/api.php?amount=10&category=12&type=multiple";
+    const queryURL = "https://opentdb.com/api.php?amount=10&category=12&type=boolean";
 
-    // AJAX call to the opentdb API
-    $.ajax({
+    function displayTrivia() {
+        // AJAX call to the opentdb API
+        $.ajax({
             url: queryURL,
             method: "GET"
         })
-        // Store all of the retrieved data inside of an object called "response"
-        .then(function(results) {
+            // Store all of the retrieved data inside of an object called "response"
+            .then(function (results) {
 
-            // Log the queryURL
-            console.log(queryURL);
+                const response = results;
+                $("#trivia-view").empty();
+                // Log the queryURL
+                console.log(response.results);
 
-            // Log the resulting object
-            console.log(results);
-        });
-    // Finish setting up at a later date
+                for (let index in response.results) {
+                    let trivia = response.results[index];
+                    let question = trivia.question;
+                    let incorrectAnswers = trivia.incorrect_answers;
+                    let correctAnswer = trivia.correct_answer;
+                    let answers = [];
+                    incorrectAnswers.push(correctAnswer);
+                    answers = incorrectAnswers;
+                    answers.sort(() => 0.5 - Math.random());
+                    console.log("Question: " + question);
+                    console.log("OptionA: ", answers[0]);
+                    console.log("OptionB: ", answers[1]);
 
-    $("button").click(function() {
+                    // Div to hold each question and answer section
+                    let questionDiv = $("<div class='question'>");
+
+                    // Create question element
+                    let pQuestion = $("<p>").html(question);
+
+                    // Create possible answer element
+                    /*                     let bOptionT = $("<button class='optionT'>").html("TRUE");
+                                        let bOptionF = $("<button class='optionF'>").html("FALSE"); */
+
+                    /*                     let bOptionT = $("<button id='buttonCSS' name='group1' data-value='true'>True</button>");
+                                        let bOptionF = $("<button id='buttonCSS' name='group1' data-value='false'>False</button>"); */
+
+                    let bOptionT = $('<input id="Radio1" type="radio" class="radio" name="group1" data-value="true" >true</input>');
+                    let bOptionF = $('<input id="Radio2" type="radio" class="radio" name="group1" data-value="false">false</input>');
+
+
+
+                    // Display question element
+                    questionDiv.append(pQuestion);
+
+                    // Display possible answer element
+                    questionDiv.append(bOptionT);
+                    questionDiv.append(bOptionF);
+
+                    // Display in HTML
+                    $("#trivia-view").append(questionDiv);
+
+
+                }
+                /*                 $(".radio").click(function () {
+                                    alert($("input:radio[name='group1']:checked").attr("data-value"));
+                
+                
+                                }); */
+            });
+    }
+
+    $("button").click(function () {
         $("button").hide();
-        let timeleft = 10;
-        let downloadTimer = setInterval(function() {
+        let timeleft = 180;
+        let downloadTimer = setInterval(function () {
             document.getElementById("timer").innerHTML = "Time Remaining: " + timeleft + " Seconds";
             timeleft -= 1;
-            //Add trivia questions from API
             if (timeleft <= 0) {
+                $("#trivia-view").hide();
                 clearInterval(downloadTimer);
                 document.getElementById("timer").innerHTML = "Time's up!";
                 document.getElementById("wins").innerHTML = "Correct Answers: " + wins;
@@ -34,15 +88,6 @@ $(function() {
                 document.getElementById("skipped").innerHTML = "Unanswered: " + skipped;
             }
         }, 1000);
+        displayTrivia();
     });
-    //Figure out Timeout later
-    /* function startTimer() {
-        $("timer").text("Time Remaining: " + timeleft + " Seconds");
-        timeleft -= 1;
-        timer = setTimeout(function() {
-            $(".status").text("Time is up!");
-            clearTimeout(timer);
-        }, 1000);
-    } */
-
 });
